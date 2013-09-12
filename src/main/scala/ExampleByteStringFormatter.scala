9410ff92f720f605881ad66c496f5426d5f65569
Valerian Barbot <vbr@zenexity.com>
2013-09-12 20:00:00 +0200

    ByteStringFormatter

---
+++ src/main/scala/ExampleByteStringFormatter.scala
@@ -0,0 +1,43 @@
+import akka.util.ByteString
+import redis.{RedisClient, ByteStringFormatter}
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+case class DumbClass(s1: String, s2: String)
+
+object DumbClass {
+  implicit val byteStringFormatter = new ByteStringFormatter[DumbClass] {
+    def serialize(data: DumbClass): ByteString = {
+      ByteString(data.s1 + "|" + data.s2)
+    }
+
+    def deserialize(bs: ByteString): DumbClass = {
+      val r = bs.utf8String.split('|').toList
+      DumbClass(r(0), r(1))
+    }
+  }
+}
+
+object ExampleByteStringFormatter extends App {
+  implicit val akkaSystem = akka.actor.ActorSystem()
+
+  val redis = RedisClient()
+
+  val dumb = DumbClass("s1", "s2")
+
+  val r = for {
+    set <- redis.set("dumbKey", dumb)
+    getDumbOpt <- redis.get[DumbClass]("dumbKey")
+  } yield {
+    getDumbOpt.map(getDumb => {
+      assert(getDumb == dumb)
+      println(getDumb)
+    })
+  }
+
+  Await.result(r, 5 seconds)
+
+  akkaSystem.shutdown()
+}
+
